5bb440522e56023fbc8f1a400ebe5e08a8dcab0c
Kevin Brown <48394009+kbrowngithub@users.noreply.github.com>
2019-12-17 14:46:08 -0700

    Updated showGame and inquirer validation

--- Word.js
+++ Word.js
@@ -2,6 +2,7 @@ var Letter = require("./Letter.js");
 
 var Word = function(word) {
     this.letters = [];
+    this.lettersRemaining = false;
     this.createWordArr = function(word) {
         word.split("").forEach(letter => {
             this.letters.push(new Letter(letter));
@@ -10,21 +11,36 @@ var Word = function(word) {
     this.createWordArr(word);
     this.getWord = function() {
         var word = "";
+        this.lettersRemaining = false;
         this.letters.forEach(letter => {
             word += letter.getLetter() + " ";
+            if(letter.getLetter() === "_") {
+                this.lettersRemaining = true;
+            }
         });
         return word;
     }
+    this.moreLetters = function() {
+        return this.lettersRemaining;
+    }
     this.guess = function(character) {
         this.letters.forEach(letter => {
-            // console.log(`calling guessLetter(): letter = ${character}`);
             letter.guessLetter(character);
         });
     }
+    this.dispStr = `XXXXXXXXXXXXXXXXXXXXXXXXXX`;
+    this.startStr = `\nWord:      `;
+    this.endStr = `  \n`;
+
+    this.showGame = function(arr, gr) {
+        console.log(`\n\n${this.dispStr}${this.dispStr}`);
+        if(arr !== undefined){console.log(`REMAINING GUESSES:  ${gr}\nGUESSED LETTERS:    ${arr.join(" ").toUpperCase()}\n`);}
+        console.log(`${this.startStr}${this.getWord()}${this.endStr}`);
+        console.log(`${this.dispStr}${this.dispStr}`);
+    }
 }
 
-var word = new Word("test");
-
-word.getWord();
+// var word = new Word("test");
+// word.getWord();
 
 module.exports = Word;
--- index.js
+++ index.js
@@ -3,54 +3,52 @@ var inquirer = require('inquirer');
 var Word = require('./Word.js');
 
 var word = randomWords().toUpperCase();
-console.log(`Current word is ${word}`);
+// console.log(`Current word is ${word}`);
 
 var currWord = new Word(word);
+
 var guesses = 9;
+var guessedLetters = [];
 
 function play(guesses) {
+    currWord.showGame(guessedLetters, guesses);
     inquirer.prompt([
         {
             type: "input",
             name: "guessedLetter",
-            message: "Type a letter to guess",
+            message: "\n\n\nType a letter to guess:  ",
             validate: function validateLetter(value) {
                 var pass = value.match(/^[A-Za-z]+$/);
-                if (pass) { return true; }
-                return "Must be a character a - z (use backspace to re-enter a valid character)";
+                if (pass) {
+                    if(guessedLetters.indexOf(value) > -1) {
+                        return "You already guessed that letter. (use backspace to enter new letter)"; 
+                    } else {
+                        guessedLetters.push(value);
+                        return true;
+                    }
+                }
+                return "Must be a character a - z (use backspace to re-enter a valid letter)";
             }
         }
     ]).then(answer => {
-        console.log(`Guessed letter is ${answer.guessedLetter.toUpperCase()}`);
+        // console.log(`Guessed letter is ${answer.guessedLetter.toUpperCase()}`);
         currWord.guess(answer.guessedLetter.toString().toUpperCase());
-        console.log(`Word Display: ${currWord.getWord()}`);
+        // console.log(`Word Display: ${currWord.getWord()}`);
+        currWord.showGame(guessedLetters, guesses);
 
-        if (guesses > 0) {
+        // console.log(`Guesses: ${guesses}, More Letters: ${currWord.moreLetters()}`);
+        if (guesses > 0 && currWord.moreLetters()) {
             guesses--;
             play(guesses);
         } else {
-            console.log('Game Over');
-            console.log(`Word Display: ${currWord.getWord()}`);
+            // console.log(`${currWord.showGame()}`);
+            if(guesses > 0) {
+                console.log('****** YOU WON! ******');
+            } else {
+                console.log(`Out of Guesses. The word was: ${word}`);
+            }
         }
     });
 }
 
 play(guesses);
-
-
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         name: "guessedLetter",
-//         message: "Type a letter to guess",
-//         validate: function validateLetter(value) {
-//             var pass = value.match(/^[A-Za-z]+$/);
-//             if (pass) { return true; }
-//             return "Must be a character a - z (use backspace to re-enter a valid character)";
-//         }
-//     }
-// ]).then(function (answer) {
-//     console.log(`Guessed letter is ${answer.guessedLetter.toUpperCase()}`);
-//     currWord.guess(answer.guessedLetter.toString().toUpperCase());
-//     console.log(`Word Display: ${currWord.getWord()}`);
-// });
